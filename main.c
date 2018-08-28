2ec87b96a5767ce4b7daf8d646db657f38da8aa0
SKGleba <30833773+SKGleba@users.noreply.github.com>
2018-08-29 00:10:04 +0200

    v6.1

--- main.c
+++ main.c
@@ -42,6 +42,7 @@
 #define gmdblk "gcd-lp-act-mediaid"
 #define gcdblk "gcd-lp-act-entire"
 #define emcblk "int-lp-act-entire"
+#define imcblk "int-lp-ign-userext"
 #define groblk "gcd-lp-ign-gamero"
 #define grwblk "gcd-lp-ign-gamerw"
 
@@ -58,11 +59,13 @@
 #define PDEVSZIX (SCE_CTRL_CROSS | SCE_CTRL_UP)
 #define PDEVSZXC (SCE_CTRL_TRIANGLE | SCE_CTRL_UP)
 #define PDEVSZCM (SCE_CTRL_LTRIGGER | SCE_CTRL_UP)
+#define PDEVBLKO (SCE_CTRL_RTRIGGER | SCE_CTRL_UP)
 #define MDEVSZCC (SCE_CTRL_CIRCLE | SCE_CTRL_DOWN)
 #define MDEVSZXX (SCE_CTRL_SQUARE | SCE_CTRL_DOWN)
 #define MDEVSZIX (SCE_CTRL_CROSS | SCE_CTRL_DOWN)
 #define MDEVSZXC (SCE_CTRL_TRIANGLE | SCE_CTRL_DOWN)
 #define MDEVSZCM (SCE_CTRL_LTRIGGER | SCE_CTRL_DOWN)
+#define MDEVBLKO (SCE_CTRL_RTRIGGER | SCE_CTRL_DOWN)
 
 #define LOG(...) \
 	do { \
@@ -111,7 +114,10 @@ char mit[][100] = {" -> vs0 ( int-lp-ign-vsh ) "," -> vd0 ( int-lp-ign-vshdata )
 char wit[][150] = {" -> Perform a factory backup"," -> Active os0 ( int-lp-act-os )"," -> Inactive os0 ( int-lp-ina-os )"," -> Active slb2 ( int-lp-act-sloader )"," -> Inactive slb2 ( int-lp-ina-sloader )"," -> idstorage ( int-lp-ign-idstor )"," -> MBR"," -> Reboot"};
 char cit[][150] = {" -> ux0 ( mcd-lp-act-userext )"," -> MediaId ( mcd-lp-act-mediaid )"," -> MBR"," -> Reboot", " -> Exit"};
 char rit[][150] = {" -> grw0 ( gcd-lp-ign-gamerw )"," -> gro0 ( gcd-lp-ign-gamero )"," -> MediaId ( gcd-lp-act-mediaid )"," -> MBR"," -> Reboot", " -> Exit"};
-char kit[][256] = {" -> Check if the device exists (BLKNAME)"," -> Dump one block"," -> Dump MBR"," -> Dump device (MASTER)"," -> Dump device (FAT16)"," -> Dump device (exFAT)"," -> Dump device (custom size)"," -> Bruteforce device names"};
+char kit[][256] = {" -> Check if the device exists (BLKNAME)"," -> Dump one block"," -> Dump MBR"," -> Dump device (MASTER)"," -> Dump device (FAT16)"," -> Dump device (exFAT)"," -> Dump device (custom size)"," -> Clone device (INT/GCD/MCD/USB)"," -> Bruteforce device names"};
+const char hw[7][32] = {"int", "ext", "gcd", "mcd", "xmc", "uma", "usd"};
+char hwdesc[7][128] = {"Internal eMMC","Device in Game Card slot","Game Card","Device in Memory Card slot","Memory Card","USB","Unknown"};
+
 int item_count = 8;
 int smodes = 6;
 int szodes = 4;
@@ -134,10 +140,15 @@ int ocdevsz = 0;
 
 int omd = 0;
 
+int cinf = 0;
+int couf = 1;
+
 int cbfsz;
 int wmode = 0;
 int smode = 0;
 int szode = 2;
+
+int rapechk = 0;
 
 int sel = 0;
 int sub_sel = 0;
@@ -197,11 +208,22 @@ int fpsz(master_block_t *master, uint8_t wpc) {
 	return prsz;
 }
 
+int chknorape(){
+sceKernelDelayThread(0.35 * 1000 * 1000);
+SceCtrlData pad;
+rapechk = 1;
+psvDebugScreenPrintf("\nPress CROSS to continue or CIRCLE to abort...\n\n");
+while (rapechk == 1){
+sceCtrlPeekBufferPositive(0, &pad, 1);
+if (pad.buttons == SCE_CTRL_CROSS) {rapechk = 0; return 0;}
+if (pad.buttons == SCE_CTRL_CIRCLE) {rapechk = 0; return 1;}}}
+
+
 void smn(){
 if (omd == 0) item_count = 8;
 if (omd == 1) item_count = 6;
 if (omd == 2) item_count = 5;
-if (omd == 3) item_count = 8;
+if (omd == 3) item_count = 9;
 	psvDebugScreenClear(COLOR_BLACK);
 	psvDebugScreenSetFgColor(COLOR_RED);
 		if (wmode == 0) psvDebugScreenPrintf("MODE:RR&R");
@@ -209,27 +231,31 @@ if (omd == 3) item_count = 8;
 		if (szode == 1) cbfsz = 0x4000;
 		if (szode == 2) cbfsz = 0x8000;
 		if (szode == 0) cbfsz = 0x2000;
-						psvDebugScreenPrintf("       BUFSZ:0x%X", cbfsz);
-	   if (omd < 4) psvDebugScreenPrintf("       DEV:%s", mddev[omd]);
-	  if (omd == 4) psvDebugScreenPrintf("       DEV:CUSTOM");
-						psvDebugScreenPrintf("      LOC:%s\n\n", ddev[smode]);
+					psvDebugScreenPrintf("       BUFSZ:0x%X", cbfsz);
+		if (omd < 4) psvDebugScreenPrintf("       DEV:%s", mddev[omd]);
+		if (omd > 3) psvDebugScreenPrintf("       DEV:CUSTOM");
+		if (omd != 5) psvDebugScreenPrintf("      LOC:%s", ddev[smode]);
 	psvDebugScreenSetFgColor(COLOR_GREEN);
-	psvDebugScreenPrintf("                           NDP v6                                \n");
-	psvDebugScreenPrintf("                         By SKGleba                              \n");
+	psvDebugScreenPrintf("\n\n                          NDP v6.1                               \n");
+		psvDebugScreenPrintf("                         By SKGleba                              \n");
 			psvDebugScreenSetFgColor(COLOR_CYAN);
 		if (smd == 0 && omd == 0) psvDebugScreenPrintf("               LIST MODE:  [SOFTWARE]  FIRMWARE                  \n");
 		if (smd == 1 && omd == 0) psvDebugScreenPrintf("               LIST MODE:   SOFTWARE  [FIRMWARE]                 \n");
 		  if (opmode == 1 && omd < 3) psvDebugScreenPrintf("              OPERATION MODE:  [BACKUP]  RESTORE                 \n");
 		  if (opmode == 0 && omd < 3) psvDebugScreenPrintf("              OPERATION MODE:   BACKUP  [RESTORE]                \n");
-						 if (omd < 3) psvDebugScreenPrintf("         PRESS [SELECT] TO PERFORM A FULL DEVICE DUMP            \n");
-					if (omd == 4) psvDebugScreenPrintf("\n\n                  PRESS [SELECT] TO GO BACK                      \n");
-						if (omd == 4) psvDebugScreenPrintf("             PRESS [START] TO ACCEPT AND CONTINUE                \n");
+		  if (omd < 3 && opmode == 1) psvDebugScreenPrintf("                 PRESS [SELECT] TO DUMP: %s                     \n", mddev[omd]);
+		  if (omd < 3 && opmode == 0) psvDebugScreenPrintf("                PRESS [SELECT] TO RESTORE: %s                   \n", mddev[omd]);
+					if (omd > 3) psvDebugScreenPrintf("\n\n                  PRESS [SELECT] TO GO BACK                      \n");
+						if (omd > 3) psvDebugScreenPrintf("             PRESS [START] TO ACCEPT AND CONTINUE                \n");
 			psvDebugScreenSetFgColor(COLOR_WHITE);
 					  if (omd == 4) psvDebugScreenPrintf("\n                     ENTER DEVICE SIZE:                          \n");
+					  if (omd == 5) psvDebugScreenPrintf("\n                 CHOOSE DEVICE(S) TO CLONE:                      \n");
 			psvDebugScreenSetFgColor(COLOR_GREEN);
 				  if (omd == 4) psvDebugScreenPrintf("\n\n\n         DEVICE SIZE (BLOCKS): 0x%X\n", ocdevsz);
+				  if (omd == 5) psvDebugScreenPrintf("\n\n\n       DEVICE (INPUT):  %s ( %s )\n", hwdesc[cinf], hw[cinf]);
 			psvDebugScreenSetFgColor(COLOR_YELLOW);
-			        if (omd == 4) psvDebugScreenPrintf("\n\n          DEVICE SIZE (FINAL): 0x%X\n", (ocdevsz * 0x200));
+				    if (omd == 5) psvDebugScreenPrintf("\n\n       DEVICE (OUTPUT): %s ( %s )\n\n", hwdesc[couf], hw[couf]);
+			        if (omd == 4) psvDebugScreenPrintf("\n\n         DEVICE SIZE (FINAL):  0x%X\n", (ocdevsz * 0x200));
 		psvDebugScreenSetFgColor(COLOR_RED);
 	for(i = 0; i < item_count; i++){
 		if(sel==i){
@@ -259,10 +285,40 @@ sprintf(utx, "%s:ndp_f/%s.img", ddev[smode], oup);}
 psvDebugScreenPrintf("Target IN: %s ( %s )...\n", utx, oup);
 psvDebugScreenPrintf("Target OUT: %s ( %s )...\n", dumpdev, oup);
 psvDebugScreenPrintf("Target size: 0x%X ( %s )...\n", devsz, oup);
+int rape = chknorape();
+if (rape == 0){
 psvDebugScreenPrintf("Working ( %s )...\n", oup);
-kndpWorkDevice(utx, dumpdev, opmode, devsz, islc, oslc, szode);
+kndpWorkDevice(utx, dumpdev, opmode, devsz, islc, oslc, szode);}
 psvDebugScreenPrintf("Done...\n");
 sceKernelDelayThread(1 * 1000 * 1000);
+return 1;
+}
+
+int ndpReWriteDev(){
+int cdevsz;
+psvDebugScreenPrintf("Preparing ( CLONE )...\n");
+sprintf(utx, "sdstor0:%s-lp-act-entire", hw[cinf]);
+sprintf(dumpdev, "sdstor0:%s-lp-act-entire", hw[couf]);
+if (exists("ur0:temp/tempmbr.x") == 1) sceIoRemove("ur0:temp/tempmbr.x");
+kndpWorkDevice(utx, "ur0:temp/tempmbr.x", 1, 0x200, 0, 0, 2);
+if (exists("ur0:temp/tempmbr.x") == 1){
+  SceUID fd;
+  static master_block_t master;
+  fd = sceIoOpen("ur0:temp/tempmbr.x", SCE_O_RDONLY, 0777);
+  sceIoRead(fd, &master, sizeof(master));
+  cdevsz = master.device_size * 0x200;
+  sceIoClose(fd);
+sceIoRemove("ur0:temp/tempmbr.x");
+psvDebugScreenPrintf("Target IN: %s ( CLONE )...\n", utx);
+psvDebugScreenPrintf("Target OUT: %s ( CLONE )...\n", dumpdev);
+psvDebugScreenPrintf("Device size: 0x%X ( CLONE )...\n", cdevsz);
+int rape = chknorape();
+if (rape == 0){
+psvDebugScreenPrintf("Working ( CLONE )...\n");
+kndpWorkDevice(utx, dumpdev, 0, cdevsz, 0, 0, szode);}
+psvDebugScreenPrintf("Done...\n");
+sceKernelDelayThread(1 * 1000 * 1000);
+}
 return 1;
 }
 
@@ -307,8 +363,10 @@ sceIoRemove("ur0:temp/tempmbr.x");
 psvDebugScreenPrintf("Target IN: %s ( %s )...\n", utx, oup);
 psvDebugScreenPrintf("Target OUT: %s ( %s )...\n", dumpdev, oup);
 psvDebugScreenPrintf("Device size: 0x%X ( %s )...\n", cdevsz, oup);
+int rape = chknorape();
+if (rape == 0){
 psvDebugScreenPrintf("Working ( %s )...\n", oup);
-kndpWorkDevice(utx, dumpdev, opmode, cdevsz, 0, 0, szode);
+kndpWorkDevice(utx, dumpdev, opmode, cdevsz, 0, 0, szode);}
 }
 psvDebugScreenPrintf("Done...\n");
 sceKernelDelayThread(1 * 1000 * 1000);}
@@ -338,8 +396,10 @@ sceIoRemove("ur0:temp/tempmbr.x");
 psvDebugScreenPrintf("Target IN: %s ( %s )...\n", utx, oup);
 psvDebugScreenPrintf("Target OUT: %s ( %s )...\n", dumpdev, oup);
 psvDebugScreenPrintf("Device size: 0x%X ( %s )...\n", cdevsz, oup);
+int rape = chknorape();
+if (rape == 0){
 psvDebugScreenPrintf("Working ( %s )...\n", oup);
-kndpWorkDevice(utx, dumpdev, opmode, cdevsz, 0, 0, szode);
+kndpWorkDevice(utx, dumpdev, opmode, cdevsz, 0, 0, szode);}
 }
 psvDebugScreenPrintf("Done...\n");
 sceKernelDelayThread(1 * 1000 * 1000);}
@@ -358,7 +418,7 @@ sceAppMgrLoadExec("app0:fapordev", NULL, NULL);} //Find Another Partition OR DEV
 
 int brutefap(){
 psvDebugScreenPrintf("Bruteforcing, please be patient.\n");
-	const char hw[7][32] = {"int", "ext", "gcd", "mcd", "uma", "usd", "xmc"};
+
 	const char tbl[2][8] = {"lp", "pp"};
 	const char act[3][16] = {"ina", "act", "ign"};
 	const char pt[17][256] = {"a", "unused", "idstor", "sloader", "os", "vsh", "vshdata", "vtrm", "user", "userext", "gamero", "gamerw", "updater", "sysdata", "mediaid", "pidata", "entire"};
@@ -418,7 +478,7 @@ if (mod_id >= 0) {const char * const argv[] = { "restart", NULL }; sceAppMgrLoad
 	while (l == 1) {
 			sceCtrlPeekBufferPositive(0, &pad, 1);
 
-			if (omd != 4){
+			if (omd < 4){
 			if (pad.buttons == SCE_CTRL_CROSS) {
 				switch (sel){
 
@@ -474,7 +534,13 @@ if (mod_id >= 0) {const char * const argv[] = { "restart", NULL }; sceAppMgrLoad
 					case 7:
 						if (smd == 0 && omd == 0) sceKernelExitProcess(0);
 						if (smd == 1 && omd == 0) scePowerRequestColdReset();
-						if (omd == 3) brutefap();
+						if (omd == 3) omd = 5;
+					break;
+					case 8:
+						if (omd == 3) brutefap(0);
+					break;
+					case 9:
+						if (omd == 3) brutefap(1);
 					break;
 				}
 				smn();
@@ -484,14 +550,22 @@ if (mod_id >= 0) {const char * const argv[] = { "restart", NULL }; sceAppMgrLoad
 			if (pad.buttons == SCE_CTRL_SELECT) { 
 				if (omd == 0) ndpWorkDevBRBasedFull(emcblk, "emmc", 0);
 				if (omd == 1) ndpWorkDevBRBasedFull(gcdblk, "gcd", 0);
-				if (omd == 2) ndpWorkDevBRBasedFull(mcdblk, "mc", 0);
+				if (omd == 2) ndpWorkDevBRBasedFull(mcdblk, "mcd", 0);
 				if (omd == 4) omd = 3;
+				if (omd == 5) omd = 3;
 				smn();
 				sceKernelDelayThread(0.35 * 1000 * 1000);}
-			if (pad.buttons == SCE_CTRL_START) { 
-				if (omd == 4) fapcall(69);}
+
+
+			if (pad.buttons == SCE_CTRL_START) {
+				if (omd == 5) ndpReWriteDev();
+				if (omd == 4) fapcall(69);
+				smn();
+				sceKernelDelayThread(0.35 * 1000 * 1000);}
+
+
 			if (pad.buttons == SCE_CTRL_RTRIGGER) {
-			if(omd != 4){
+			if(omd < 4){
 				if (wmode == 1){
 					if (opmode == 1) { opmode = 0;
 					} else { opmode = 1; }sceKernelDelayThread(0.35 * 1000 * 1000);smn();}
@@ -500,12 +574,12 @@ if (mod_id >= 0) {const char * const argv[] = { "restart", NULL }; sceAppMgrLoad
 					} else { opmode = 1; }sceKernelDelayThread(0.35 * 1000 * 1000);smn();}}
 			if (omd == 4) {ocdevsz = 0;smn();}}
 			if (pad.buttons == SCE_CTRL_LTRIGGER) {
-			if(omd != 4){
+			if(omd < 4){
 				if (smd == 1) { smd = 0;
 			} else { smd = 1; }sceKernelDelayThread(0.35 * 1000 * 1000);smn();}}
 			
 			if (pad.buttons == SCE_CTRL_UP) {
-			if (omd != 4){
+			if (omd < 4){
 				if(sel!=0){
 					sel--;
 					sub_sel = 0;
@@ -519,7 +593,7 @@ if (mod_id >= 0) {const char * const argv[] = { "restart", NULL }; sceAppMgrLoad
 			}
 			
 			if (pad.buttons == SCE_CTRL_DOWN) {
-			if (omd != 4){
+			if (omd < 4){
 				if(sel+1<item_count){
 					sel++;
 					sub_sel = 0;
@@ -613,6 +687,16 @@ if (mod_id >= 0) {const char * const argv[] = { "restart", NULL }; sceAppMgrLoad
 				ocdevsz = ocdevsz + 0x100000;
 				smn();
 				sceKernelDelayThread(0.35 * 1000 * 1000);}
+				if(omd==5){
+				if (cinf+1 != 7) cinf++;
+				smn();
+				sceKernelDelayThread(0.35 * 1000 * 1000);}
+			}
+			if (pad.buttons == PDEVBLKO) {
+				if(omd==5){
+				if (couf+1 != 7) couf++;
+				smn();
+				sceKernelDelayThread(0.35 * 1000 * 1000);}
 			}
 
 			if (pad.buttons == MDEVSZIX) {
@@ -642,6 +726,17 @@ if (mod_id >= 0) {const char * const argv[] = { "restart", NULL }; sceAppMgrLoad
 			if (pad.buttons == MDEVSZCM) {
 				if(omd==4){
 				ocdevsz = ocdevsz - 0x100000;
+				smn();
+				sceKernelDelayThread(0.35 * 1000 * 1000);}
+				if(omd==5){
+				if (cinf > 0) cinf--;
+				smn();
+				sceKernelDelayThread(0.35 * 1000 * 1000);}
+			}
+			if (pad.buttons == MDEVBLKO) {
+				if(omd==5){
+				if (wmode == 0){if (couf > 1) couf--;}
+				if (wmode == 2){if (couf > 0) couf--;}
 				smn();
 				sceKernelDelayThread(0.35 * 1000 * 1000);}
 			}
